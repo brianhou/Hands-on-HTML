472cad4e5d1fd17e9466069ec2776efc9094432a
brianhou <brian.hou@live.com>
2014-04-05 16:47:19 -0700

    Rohan is *literally* the worst.

--- hack.py
+++ hack.py
@@ -8,30 +8,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
 def generate_page(json_file):
-    """Takes in the json representation of the page and returns HTML."""
-    jason = json.loads(json_file)
-    image0 = {
-        'top': 0,
-        'left': 0,
-        'width': 0.3,
-        'aspect-ratio': 2,
-        'path': url_for('static', filename='images/the.jpg')
-    }
-    for image in jason['images'].values():
-        new_img = url_for('static', filename=image['path'])
-        image['path'] = new_img
-    # jason['images'][2] = image0
-    # jason['num_images'] += 1
-    return render_json(jason)
+  """Takes in the json representation of the page and returns HTML."""
+  jason = json.loads(json_file)
+  image0 = {
+    'top': 0,
+    'left': 0,
+    'width': 0.3,
+    'aspect-ratio': 2,
+    'path': url_for('static', filename='images/the.jpg')
+  }
+  for image in jason['images'].values():
+    new_img = url_for('static', filename=image['path'])
+    image['path'] = new_img
+  # jason['images'][2] = image0
+  # jason['num_images'] += 1
+  return render_json(jason)
 
 @app.route('/render/')
 def render():
-    # uploads image
-    img = 'images/test'
-    return generate_page(jsonify(img))
+  # uploads image
+  img = 'images/test'
+  return generate_page(jsonify(img))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
--- render.py
+++ render.py
@@ -1,15 +1,15 @@
 from yattag import Doc
 
 def render_json(data):
-    doc, tag, text = Doc().tagtext()
-    def create_div(photo_id, top, left, width, aspect_ratio, path):
-        width = '{}%'.format(100 * width)
-        with tag('div'):
-            doc.stag('img', width=width, src=path, id=photo_id)
+  doc, tag, text = Doc().tagtext()
+  def create_div(photo_id, top, left, width, aspect_ratio, path):
+    width = '{}%'.format(100 * width)
+    with tag('div'):
+      doc.stag('img', width=width, src=path, id=photo_id)
     num_files = data['num_images']
-    for image_id, datum in data['images'].items():
-        top, left = datum['top'], datum['left']
-        width, aspect = datum['width'], datum['aspect-ratio']
-        path = datum['path']
-        create_div(str(image_id), top, left, width, aspect, path)
-    return doc.getvalue()
+  for image_id, datum in data['images'].items():
+    top, left = datum['top'], datum['left']
+    width, aspect = datum['width'], datum['aspect-ratio']
+    path = datum['path']
+    create_div(str(image_id), top, left, width, aspect, path)
+  return doc.getvalue()
