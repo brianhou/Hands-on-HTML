e28182f65ded90e31d5f8e731fdf9a79ad388222
Rohan <ronuchit@yahoo.com>
2014-04-05 15:43:06 -0700

    add json support, clean up code

--- process_input.py
+++ process_input.py
@@ -2,7 +2,7 @@ import json
 import cv2
 import numpy as np
 
-AREA_THRESHOLD = 10000
+AREA_THRESHOLD = 20000
 
 
 class ProcessInput:
@@ -14,18 +14,25 @@ class ProcessInput:
     upper_thresh = np.array([180, 100, 100], dtype=np.uint8)
     black_mask = cv2.inRange(hsv, lower_thresh, upper_thresh)
     _, contours, _ = cv2.findContours(black_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(img, contours, -1, (255, 0, 0), 3)
+    # cv2.drawContours(img, contours, -1, (255, 0, 0), 3)
     largest_area = max([cv2.contourArea(contour) for contour in contours])
+    rects = []
     image_info = {}
 
     for contour in contours:
-      # if cv2.contourArea(contour) > AREA_THRESHOLD:
-      if cv2.contourArea(contour) == largest_area:
-        x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(img, (x, y), (x+w, y+h), color=(0, 0, 255), thickness=3)
-        out_img_name = jpg_name + "_out.jpg"
-        cv2.imwrite(out_img_name, img[y:y+h, x:x+w])
-        image_info[len(image_info)] = {"top": y, "left": x, "width": w * 1.0 / img.shape[1], "aspect-ratio": w * 1.0 / h, "path": out_img_name}
+      if cv2.contourArea(contour) > AREA_THRESHOLD:
+      # if cv2.contourArea(contour) == largest_area:
+        rects.append(cv2.boundingRect(contour))
+    for x, y, w, h in rects:
+      for x2, y2, w2, h2 in rects:
+        if x > x2 and x + w < x2 + w2 and y > y2 and y + h < y2 + h2:
+          print("Removing contained rectangle")
+          rects.remove((x, y, w, h))
+    for i, (x, y, w, h) in enumerate(rects):
+      cv2.rectangle(img, (x, y), (x+w, y+h), color=(0, 0, 255), thickness=3)
+      out_img_name = jpg_name + "_out%d.jpg"%i
+      cv2.imwrite(out_img_name, img[y:y+h, x:x+w])
+      image_info[len(image_info)] = {"top": y, "left": x, "width": w * 1.0 / img.shape[1], "aspect-ratio": w * 1.0 / h, "path": out_img_name}
 
     json_obj = {"num_images": len(image_info), "images": image_info}
     print json.dumps(json_obj)
