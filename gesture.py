f98b68711b3fdb3a8f6afffbb36769c49cc87ded
Rohan <ronuchit@yahoo.com>
2014-04-05 23:55:21 -0700

    finished zoom in and out

--- gesture.py
+++ gesture.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
+import time
 
-PALM_CONCAVITY_DIST_THRESH = 8000
+PALM_CONCAVITY_DIST_THRESH = 7000
 
 
 class GestureRecognition:
@@ -9,6 +10,8 @@ class GestureRecognition:
     cap = cv2.VideoCapture(0)
     old_pos = []
     new_pos = []
+    old_dists = []
+    new_dists = []
     while cap.isOpened():
       ret, img = cap.read()
       imageYCrCb = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
@@ -54,17 +57,39 @@ class GestureRecognition:
       defects = cv2.convexityDefects(cnt, hull)
       if defects is None:
         continue
+      concave_points = []
       for i in range(defects.shape[0]):
         s, e, f, d = defects[i, 0]
         start = tuple(cnt[s][0])
         end = tuple(cnt[e][0])
         far = tuple(cnt[f][0])
-        if (far[0] - cx) ** 2 + (far[1] - cy) ** 2 < PALM_CONCAVITY_DIST_THRESH:
+        concave_points.append(far)
+        if self.get_distance(far, (cx, cy)) < PALM_CONCAVITY_DIST_THRESH:
           cv2.line(img, start, end, [0, 255, 0], 2)
           cv2.circle(img, far, 5, [0, 0, 255], -1)
+      concave_points.sort(key=lambda p: p[0])
+      dist = 0
+      for i in range(1, len(concave_points)):
+        dist += self.get_distance(concave_points[i-1], concave_points[i])
+      dist /= len(concave_points)
+      if len(old_dists) < 10:
+        old_dists.append(dist)
+      if len(new_dists) < 10:
+        new_dists.append(dist)
+      else:
+        if np.percentile(np.array(new_dists), 10) > max(old_dists):
+          print "zoom in"
+        if np.percentile(np.array(new_dists), 90) < min(old_dists):
+          print "zoom out"
+        old_dists.pop(0)
+        old_dists.append(new_dists.pop(0))
+        new_dists.append(dist)
 
       cv2.imshow('input', img)
       cv2.waitKey(3)
+
+  def get_distance(self, pos1, pos2):
+    return (pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2
 
 
 if __name__ == "__main__":
